99ff163d29f085761f74eea1b4b664b73e40d021
MrBrokenHeart666 <67345838+MrBrokenHeart666@users.noreply.github.com>
2020-07-10 19:33:26 +0800

    Add files via upload

---
+++ hackwifi.py
@@ -0,0 +1,38 @@
+import os
+import mechanize
+import requests 
+import future
+os.system("clear")
+os.system("figlet Wifi-Hacker")
+os.system("print +========================!!========================+")
+os.system("echo No System Is Safe")
+os.system("date | lolcat")
+os.system("print +========================!!========================+")
+print("""
+1).Install RouterSploit
+2).About This Tool
+3).Quit
+""")
+code = raw_input("Please Choose : ")
+if code =="1":
+	print("Youre Going To Install RouterSploit")
+	aki = raw_input("[ Y/N ]")
+    	if aki =="y":
+    	    print("WARNING!!!")
+            print("Installation In Progress")
+            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/reverse-shell/routersploit.git")
+elif code =="2":
+    print("+========================!!========================+")
+    print("This Tool Was Created By MR.BR0K3NH34RT And u0_a117")
+    print("+========================!!========================+")
+    print("This Tool Is Called Routersploit")
+    print("")
+    print("""
+    What Is Routersploit?
+    I Dont Even Know
+    """)
+
+else :  
+    print("Bye Script Kiddie")
+    print("Have A Nice Day")
+    print("")
